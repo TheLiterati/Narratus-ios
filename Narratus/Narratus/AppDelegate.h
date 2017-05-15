9bdd811e089ccf541cf46d5e4bd4a7076fe6d5ab
cleex1004 <cleex1004@gmail.com>
2017-05-15 10:52:56 -0700

    inital setup

---
+++ Narratus/Narratus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Narratus
+//
+//  Created by Christina Lee on 5/15/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
