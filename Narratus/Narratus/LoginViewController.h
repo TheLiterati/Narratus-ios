4249f22ce7309dba4a0629d2e79e75bc8243523c
Christina Lee <cleex1004@gmail.com>
2017-05-16 09:36:30 -0700

    Revert "Google login st"

--- Narratus/Narratus/LoginViewController.h
+++ Narratus/Narratus/LoginViewController.h
@@ -7,12 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-@import GoogleSignIn;
 
-@interface LoginViewController : UIViewController <GIDSignInUIDelegate>
-
-
-
-
+@interface LoginViewController : UIViewController
 
 @end
